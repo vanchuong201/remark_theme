1603dc233ad27d2ff4b9c1c320be6b2ab6293ef3
chuongnv <vanchuong201>
2017-11-01 01:09:42 +0700

    push theme

---
+++ .src/gulp/options/skins.js
@@ -0,0 +1,13 @@
+// config
+var config = require('../../config.json');
+var styles = require('./styles');
+
+styles.sass.includePaths = [
+  config.source.skins + '/scss',
+  config.source.skins,
+  config.source.sass,
+  config.bootstrap.sass,
+  config.bootstrap.mixins
+];
+
+module.exports = styles;
